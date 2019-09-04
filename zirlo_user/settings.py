390a0f99cfdfb31eaf6396c0ac2cb4912a89b276
1433428978@qq.com <1433428978@qq.com>
2019-09-04 16:00:11 +0800

    change

--- zirlo_user/settings.py
+++ zirlo_user/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'zirlo_user',
-        'USER': 'sunshine',
+        'USER': 'root',
         'PASSWORD': 'gao20050203',
         'HOST': '127.0.0.1',
         'PORT': '3306',
