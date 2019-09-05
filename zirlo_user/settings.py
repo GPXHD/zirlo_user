07e6d5813c50fd1758d13ad281060125748037d8
1433428978@qq.com <1433428978@qq.com>
2019-09-05 14:45:10 +0800

    change

--- zirlo_user/settings.py
+++ zirlo_user/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'pb%#n90vc5%(r(=&+5i#9plo1ou@c)4$1%g8t+5(s=i1q^!==o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['www.zr.com', '192.168.52.130']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
