66db3415d61d029c0a2205c64553222a5ea94275
1433428978@qq.com <1433428978@qq.com>
2019-09-05 22:07:45 +0800

    change

--- zirlo_user/settings.py
+++ zirlo_user/settings.py
@@ -90,6 +90,10 @@ DATABASES = {
         'PASSWORD': 'gao20050203',
         'HOST': '127.0.0.1',
         'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"',
+            'charset': 'utf8mb4'
+        }
 
     }
 }
