cf741902285ab6b9ac039330e8e95754a6e69e89
1433428978@qq.com <1433428978@qq.com>
2019-09-11 17:39:38 +0800

    change

--- login/others/send_mail.py
+++ login/others/send_mail.py
@@ -14,8 +14,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'zrilo_user.settings.develop'
 
 def send_mails(email_add, code):
 
-    smtp_server = settings.EMAIL_HOST
-    smtp_user = settings.EMAIL_HOST_USER
+    smtp_server = 'smtp.163.com'
+    smtp_user = 'Gaopengxiang18@163.com'
     smtp_pwd = 'gao2005'
 
     sender = smtp_user
