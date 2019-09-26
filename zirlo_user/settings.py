88447786064da7b5955558ef6464e3bbbf5e98de
1433428978@qq.com <1433428978@qq.com>
2019-09-26 15:49:42 +0800

    change

--- zirlo_user/settings.py
+++ zirlo_user/settings.py
@@ -136,10 +136,10 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "statics"),
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
