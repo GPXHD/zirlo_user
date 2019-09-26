fe33fdf5b454859dbbf74d9946bf3565e297d6f7
1433428978@qq.com <1433428978@qq.com>
2019-09-26 19:52:41 +0800

    change

--- zirlo_user/settings.py
+++ zirlo_user/settings.py
@@ -137,7 +137,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+STATIC_ROOT = '/srv/zirlo_user/statics/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
