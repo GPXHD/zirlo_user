a981a00248cff579912079d74416765731a15d84
1433428978@qq.com <1433428978@qq.com>
2019-12-26 16:13:08 +0800

    change

--- zirlo_user/settings.py
+++ zirlo_user/settings.py
@@ -175,11 +175,11 @@ HAYSTACK_SEARCH_RESULT_PER_PAGE = 5
 HAYSTACK_CUSTOM_HIGHLIGHTER = 1
 
 # 会话cookie可以在用户浏览器中保持有效期, True：关闭浏览器，则Cookie失效.
-SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
-SESSION_COOKIE_DOMAIN = None             # Session的cookie保存的域名（默认）
-SESSION_COOKIE_SECURE = False            # 是否Https传输cookie（默认）
-SESSION_COOKIE_HTTPONLY = True           # 是否Session的cookie只支持http传输（默认）
-SESSION_COOKIE_AGE = 6000                # Session的cookie失效日期（2周）（数字为秒数）（默认）
+# SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+# SESSION_COOKIE_DOMAIN = None             # Session的cookie保存的域名（默认）
+# SESSION_COOKIE_SECURE = False            # 是否Https传输cookie（默认）
+# SESSION_COOKIE_HTTPONLY = True           # 是否Session的cookie只支持http传输（默认）
+# SESSION_COOKIE_AGE = 6000                # Session的cookie失效日期（2周）（数字为秒数）（默认）
 # SESSION_EXPIRE_AT_BROWSER_CLOSE = True   # 是否关闭浏览器使得Session过期（默认）
 # SESSION_SAVE_EVERY_REQUEST = True        # 是否每次请求都保存Session，默认修改之后才保存（默认）
 
